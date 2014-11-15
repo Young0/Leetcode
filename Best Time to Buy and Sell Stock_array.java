0e6705a38be14e7abcac1862ae588b9ab075571c
Youuung <yangcui09@gmail.com>
2014-11-15 13:20:38 -0500

    Update Best Time to Buy and Sell Stock_array.java

--- Best Time to Buy and Sell Stock_array.java
+++ Best Time to Buy and Sell Stock_array.java
@@ -1,10 +1,9 @@
 public class Solution {
-    public int maxProfit(int[] prices) {
+    public int maxProfit(int[] prices){
        int profit = 0;
        int cur = 0;
        for(int i = 0; i < prices.length - 1; i++){
-           int diff = prices[i+1] - prices[i];
-           cur = Math.max(diff, cur + diff);
+           cur = Math.max(0, cur + prices[i+1] - prices[i]);
            profit = Math.max(cur, profit);
        }
        return profit;
