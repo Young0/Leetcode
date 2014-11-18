f140d28eab4d3a7f1b43b6d9af52ac82ff7475d3
Youuung <yangcui09@gmail.com>
2014-11-18 13:28:24 -0500

    Update Palindrome Partitioning II_Permutation.java

--- Palindrome Partitioning II_Permutation.java
+++ Palindrome Partitioning II_Permutation.java
@@ -1,5 +1,8 @@
-import java.lang.Math;
+//There are two ways to check Palindrome, one is using recursion
+//the other is iterate all the center point then expand to both sides
 
+// Recursion way O(n^2) time and O(n^2) space
+import java.lang.Math;
 public class Solution {
     public int minCut(String s) {
         if(s.length() == 0 || s == null) return 0;
@@ -25,3 +28,17 @@ public class Solution {
         return cut[0]-1;
     }
 }
+
+
+//O(n^2) time and O(n) space but much quicker artually
+public int minCut(String s) {
+        if(s.length()==0)return 0;
+        int[]c=new int[s.length()+1];
+        for(int i=0;i<s.length();i++)c[i]=Integer.MAX_VALUE;
+        c[s.length()]=-1;
+        for(int i=s.length()-1;i>=0;i--){
+            for(int a=i,b=i;a>=0 && b<s.length() && s.charAt(a)==s.charAt(b);a--,b++) c[a]=Math.min(c[a],1+c[b+1]);
+            for(int a=i,b=i+1;a>=0 && b<s.length() && s.charAt(a)==s.charAt(b);a--,b++) c[a]=Math.min(c[a],1+c[b+1]);
+        }
+        return c[0];
+    }
